ca37a354fbb53bf3c005cb3ca25060c3d07ef0ae
ABOULAYE-tuo <dofereaboulayet@gmail.com>
2026-03-07 11:17:13 +0000

    Update main.py
    depot de l'exercice

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+liste_fruit = ["pomme", "banane", "orange"]
+liste_fruit.appent("kiwi")
+liste_fruit.remove("orange")
+liste_fruit[2] = ananas
+len(liste_fruit)
+sort(liste_fruit)
